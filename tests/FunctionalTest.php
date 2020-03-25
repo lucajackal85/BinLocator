63b7b2ec45855c7248fb8534b754506a55f1cbf9
Luca <luca.giacalone@edimotive.com>
2020-03-25 16:13:58 +0100

    added travis

--- tests/FunctionalTest.php
+++ tests/FunctionalTest.php
@@ -5,6 +5,7 @@ namespace Jackal\BinLocator\test;
 
 
 use Jackal\BinLocator\BinLocator;
+use Jackal\BinLocator\Exception\BinLocatorException;
 use PHPUnit\Framework\TestCase;
 
 class FunctionalTest extends TestCase
@@ -13,5 +14,14 @@ class FunctionalTest extends TestCase
 
         $binLocator = new BinLocator('bash');
         $this->assertEquals('/bin/bash',$binLocator->locate());
+    }
+
+    public function testRaiseExceptionOnBinNotFound(){
+
+        $this->expectException(BinLocatorException::class);
+        $this->expectExceptionMessage('Executable "not-found" has not been found on system');
+
+        $binLocator = new BinLocator('not-found');
+        $binLocator->locate();
     }
 }
